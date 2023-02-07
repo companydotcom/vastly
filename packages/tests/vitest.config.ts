31c49d0288440d3c5a965c4dd1da3ea0601704ce
Nhu Mai <nhu.mai@company-corp.com>
2023-02-06 22:58:54 -0800

    ATX-2333 | pre commit hook testing

--- packages/tests/vitest.config.ts
+++ packages/tests/vitest.config.ts
@@ -2,6 +2,6 @@ import { configDefaults, defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, "packages/template/*"],
+    exclude: [...configDefaults.exclude],
   },
 })
