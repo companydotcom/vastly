276aa7c90babf101a0d126c85e7e9c7209dc75b8
jacob.granberry <jacobgranberry@gmail.com>
2023-02-07 13:08:49 -0600

    ATX-2349 | idk man

--- apps/workshop/stories/pagination.stories.tsx
+++ apps/workshop/stories/pagination.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { Pagination } from "@dxp/ui"
+import { Pagination, usePagination, PaginationContainer, PaginationItem } from "@dxp/ui"
 
 const meta: Meta<typeof Pagination> = {
   component: Pagination,
@@ -9,5 +9,17 @@ export default meta
 type Story = StoryObj<typeof Pagination>
 
 export const Primary: Story = {
-  render: () => <Pagination primary label="Button" />,
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { items } = usePagination({ count: 8 })
+    return (
+      <Pagination>
+        <PaginationContainer>
+          {items.map((item) => (
+            <PaginationItem {...item} />
+          ))}
+        </PaginationContainer>
+      </Pagination>
+    )
+  },
 }
