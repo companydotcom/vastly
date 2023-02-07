f7820c41f2ad06560bbf2885d506fb1473337fd9
Nhu Mai <nhu.mai@company-corp.com>
2023-02-06 23:26:26 -0800

    ATX-2333 | pre commit hook vitest testing

--- packages/tests/sum/sum.test.ts
+++ packages/tests/sum/sum.test.ts
@@ -1,8 +1,6 @@
 import { sum } from "./sum"
-// import { test, expect } from "vitest"
+import { test, expect } from "vitest"
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3)
 })
-
-// test
