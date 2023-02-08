7126eb6ae3b0a1b4d853bf321b3f364881b0c081
jacob.granberry <jacobgranberry@gmail.com>
2023-02-08 09:59:55 -0600

    ATX-2349 | removes ported fields

--- packages/forms/src/components/input-field.tsx
+++
@@ -1,9 +0,0 @@
-import { FormControl, FormLabel, Skeleton, FormHelperText } from "@chakra-ui/react"
-
-export const InputField = () => {
-  return (
-    <FormControl>
-      <FormLabel htmlFor="email">Email address</FormLabel>
-    </FormControl>
-  )
-}
