6f83dd1e793db09e6cd0b5628355234ea6a57cc1
Nhu Mai <nhu.mai@company-corp.com>
2023-02-26 16:16:10 -0800

    ATX-2350 | cli

---
+++ packages/cli/src/index.tsx
@@ -0,0 +1,24 @@
+import { Command } from "commander"
+import inquirer from "inquirer"
+import { loginQuestions, generateQuestions } from "./questions/inquirer.js"
+
+//npx ts-node --esm src/index.tsx --help
+const program = new Command()
+
+program
+  .description("Custom Offering Core CLI")
+  .option("-l, --login", "Login to Company.com CLI")
+  .option("-g, --generate", "Generate new custom offering")
+  .parse(process.argv)
+
+const options = program.opts()
+
+if (options.login) {
+  inquirer.prompt(loginQuestions).then((answers) => {
+    console.log(answers)
+  })
+} else if (options.generate) {
+  inquirer.prompt(generateQuestions).then((answers) => {
+    console.log(answers)
+  })
+}
