724cba81ed822e6b1d61ccf5b8eaa771b22e497c
Sierra Wendt <serigrace@gmail.com>
2023-03-22 09:43:07 -0700

    ATX-2399 | cleans up parameter store prototype

--- packages/cli/src/secrets-manager/index.ts
+++ packages/cli/src/secrets-manager/index.ts
@@ -1,18 +1,11 @@
 import { Command } from "commander"
 import { SSMClient, GetParametersByPathCommand } from "@aws-sdk/client-ssm"
-// import { spawn, spawnSync, ChildProcess } from "child_process"
 import ora from "ora"
 import chalk from "chalk"
 import { writeFile } from "fs"
 
 const client = new SSMClient({
   region: "us-east-1",
-  credentials: {
-    accessKeyId: "ASIAW7S7QFIRPXN24HM2",
-    secretAccessKey: "xbdXZPpTpq+xJPYCKsfsjb4VeCbGBUO5dZgYKBr0",
-    sessionToken:
-      "IQoJb3JpZ2luX2VjEG8aCXVzLWVhc3QtMSJHMEUCIBgXZlDnzXoio1bIFI7gLMeKtLKC1R0PaM2gLtxqqUvqAiEA1XGzFZgykKUNiNfB9sTxp5/74FIxxT6BK+eVjYf2+7EqmAMIOBAAGgw0ODAxNjI4ODQxMzAiDE/uVcTcIJ/SVGMZfyr1Ah8cmZT7yWNuCNcEQMcFUau3HEoMsVNMRuWTb8ONIyjEERFI2HNgze3pyCotprwN5p1GA2BehhH9WWCWhDfeQh0MFHrzIkhblBXN+v0zGoUswfved+vNmLZkZYksgkWfv5zvC7ze6YABNriRpaOFFg8zvXyower/Lp5U5hbZRW5LmEzMe33WdNDeuA0LSlCD/u0WU8RiI/6T6FP2GmvDeUxBi6QSCyLq9vKDBITpA9X5w3awWKahGTJMDGLLCjtHOHHR4Yfjh+C8m96CQ6/PZhVnb2lbIDfBYFAYYkeIP57z357ri6ZpkrLTKPifzqLGJ/xG8HlWyTtnOvJCP3UJATUXv6kzvVGbrY3Xd454/yhcO0z128mvlInGHB/LqAIUUYtLgHFWIgV7ynDY6lW6NkWz46aY9In/r39yMR/PHVUulhesLFIyeTbsF+UDbF3c4/zyiycFN4bq5dJNipE1nhaTxFH95E8nkAncNSlcZafRmlw0xG0wx9/ToAY6pgFSb6XGbvDuWw2aQAZyQv97HSp+mjgbHNxm4vORZBW8v456x7I63OtKH0cJt+J3ZtjTBrLOQWXX5rG1KJ4SoCjjWOKNElH+x/OG71mbycaXAJ0N25Zzd6rTIz5hIw0OtkHKXCaLeWBrsuFLTsMDchWgk8rw3oQFTId5CLWmvKpV9+vzN8sDwIaHxOx2sqfkR5mmZAIBBskuPKZpoJK2bNQcgiBYVQad",
-  },
 })
 
 const program = new Command()
