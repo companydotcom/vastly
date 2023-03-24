d895de3b27bbaa9e369e4df3bdf080b11865b2e3
Sierra Wendt <serigrace@gmail.com>
2023-03-23 17:07:23 -0700

    ATX-2399 | pivot to s3, testing add/create/pull commands via CLI

---
+++ packages/cli/src/env-manager/index-s3.ts
@@ -0,0 +1,189 @@
+import { Command } from "commander"
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  // ListBucketsCommand,
+  CreateBucketCommand,
+} from "@aws-sdk/client-s3"
+import ora from "ora"
+import chalk from "chalk"
+import inquirer from "inquirer"
+import { writeFile } from "fs"
+
+const client = new S3Client({
+  region: "us-east-1",
+})
+
+const program = new Command()
+
+program
+  .description("Environment variables manager")
+  .option("-p, --pull <env>", "Must specify an environment. For example: prod or dev")
+  .option("-l, --list <env>", "List all environment variables")
+  .option("-a, --add <env>", "Add a value to a specific environment")
+  .option("-d, --delete <env>", "Remove a value to a specific environment")
+  .parse(process.argv)
+
+const options = program.opts()
+
+if (options.list) {
+  console.log(chalk.bgCyanBright(`Nothing to list! Come back soon :)`))
+}
+if (options.add) {
+  const addVariableToS3 = async () => {
+    // cannot append to an existing file in s3
+    // pull down file from s3 aka check it's existence
+    // if exists
+    // copy contents locally and add the key/value pair obtained by CLI prompts
+    // use PutObject to overwrite existing file
+    // if not
+    // use PutObject to create new file
+    try {
+      // check is envs bucket exists already
+      // if so,
+      // copy current object
+      // create new object: Ask for key/value from CLI
+      // combine both old and new and submit
+
+      // const listBuckets = new ListBucketsCommand({})
+      // const { Buckets } = await client.send(listBuckets)
+      // const envBucket = Buckets.find((bucket) => bucket.Name === "dxp-uniquevalue-envs")
+      // if (Buckets.length && envBucket) {
+
+      // } else {
+      const spinner = ora({
+        text: `Creating env store...`,
+        color: "magenta",
+      }).start()
+      // if bucket already exists, nothing happens ??
+      const createBucket = new CreateBucketCommand({
+        Bucket: "dxp-uniquevalue-envs",
+        ObjectOwnership: "BucketOwnerEnforced",
+      })
+      const response = await client.send(createBucket)
+      console.log("🚀 ~ file: index-s3.ts:65 ~ addVariableToS3 ~ response:", response)
+      // if (Location) {
+      //   spinner.succeed(chalk.green(`Store successfully created!`))
+
+      //   const createObjSpinner = ora({
+      //     text: "Storing your variables...",
+      //     color: "magenta",
+      //   }).start()
+
+      //   const createEnvObj = new PutObjectCommand({
+      //     Body: `${options.add}=ThisIsATest`,
+      //     Bucket: "dxp-uniquevalue-envs",
+      //     Key: `${options.add}.txt`,
+      //     ServerSideEncryption: "AES256",
+      //     Tagging: `environment=${options.add}`,
+      //   })
+      //   const { ETag } = await client.send(createEnvObj)
+      //   if (ETag)
+      //     createObjSpinner.succeed(chalk.bgGreenBright(`Success! Your data has been stored.`))
+      // }
+      spinner.stop()
+      // }
+    } catch (error) {
+      console.log("🚀 ~ file: index-s3.ts:97 ~ addVariableToS3 ~ error:", error)
+    }
+  }
+  addVariableToS3()
+}
+
+if (options.pull) {
+  const getVariables = async () => {
+    const env = options.pull
+    const spinner = ora({
+      text: `Pulling variables...`,
+      color: "magenta",
+    }).start()
+
+    try {
+      const initialCommand = new GetObjectCommand({
+        Bucket: "dxp-uniquevalue-envs",
+        Key: `${env}.txt`,
+      })
+
+      const { Body, $metadata } = await client.send(initialCommand)
+      const responseToString = await Body.transformToString()
+
+      if ($metadata.httpStatusCode === 200) {
+        spinner.succeed(chalk.green(`Variables for ${env} successfully fetched`))
+
+        const createFileSpinner = ora({
+          text: "Creating .env file...",
+          color: "magenta",
+        }).start()
+        writeFile(`./.env`, responseToString, (err) => {
+          if (err) throw new Error()
+        })
+        createFileSpinner.succeed(chalk.bgGreenBright(`File successfully created!`))
+      } else {
+        throw new Error()
+      }
+    } catch (error) {
+      console.log("🚀 ~ file: index-s3.ts:127 ~ getVariables ~ error:", error)
+      spinner.stop()
+      if (error.Code === "NoSuchKey") {
+        console.log(
+          chalk.bgRedBright(
+            `${error.Code}: There are no variables stored for this environment. Use "--help" to see available options`,
+          ),
+        )
+      }
+      if (error.Code === "ExpiredToken") {
+        console.log(
+          chalk.bgYellowBright(
+            `${error.Code}: Your token has expired. Please refresh your credentials.`,
+          ),
+        )
+      }
+      if (error.Code === "NoSuchBucket") {
+        console.log(
+          chalk.bgYellowBright(
+            `${error.Code}: You haven't created a environment store! Run 'pnpm run env --add <env>' to set up your store`,
+          ),
+        )
+      }
+    }
+  }
+  getVariables()
+}
+
+if (options.delete) {
+  // cannot append or edit an existing file in s3
+  // pull down data and store locally/copy it
+  // using user input to get a key, delete the necessary line
+  // use PutObject command to overwrite existing file in s3
+  const deleteVariable = async () => {
+    const env = options.delete
+    const initialCommand = new GetObjectCommand({
+      Bucket: "dxp-uniquevalue-envs",
+      Key: `${env}.txt`,
+    })
+
+    const { Body, $metadata } = await client.send(initialCommand)
+    const responseToString = await (await Body.transformToString()).split("=")
+    console.log("🚀 ~ file: index-s3.ts:161 ~ deleteVariable ~ responseToString:", responseToString)
+  }
+  deleteVariable()
+}
+/**
+ * TODO:
+ * Proper error handling, authentication, .env file placement, add/delete will need prompts like key/value to add & key to delete
+ */
+
+const convertToEnv = (allParams: any) => {
+  let envFile = ""
+  const result = {}
+  allParams.map((parameter: any) => {
+    if (parameter.Name.includes("region")) {
+      result["AWS_REGION"] = parameter.Value
+    }
+  })
+  for (const key of Object.keys(result)) {
+    envFile += `${key}=${result[key]}\n`
+  }
+  return envFile
+}
