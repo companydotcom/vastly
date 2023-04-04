3958256d81479d0f21b0b8f908e96b3508cab394
ctbw <ctbwilhite@gmail.com>
2023-04-04 16:03:38 -0500

    ATX-2405 | bumps version and fixes pathing issue

--- packages/create-dxp-app/src/utils/generate-local-repo.ts
+++ packages/create-dxp-app/src/utils/generate-local-repo.ts
@@ -1,9 +1,10 @@
+import * as path from "path"
 import { spawnSync } from "child_process"
 import { copyTemplate } from "./copy-template.js"
 import { GenerateAnswers } from "../types"
 
 export const generateLocalRepo = async (answers: GenerateAnswers) => {
-  spawnSync("mkdir", [answers.repoName])
+  spawnSync("mkdir", [path.join(process.cwd(), answers.repoName)])
   process.chdir(answers.repoName)
   copyTemplate(answers.packageManager)
 }
