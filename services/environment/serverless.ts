6ff6bc2af2090f7a34e58de28abe92d1122a0a79
Sierra Wendt <serigrace@gmail.com>
2023-04-13 12:14:42 -0700

    ATX-2428 | fixes folder name

--- services/environment/serverless.ts
+++ services/environment/serverless.ts
@@ -18,9 +18,6 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
-      TABLE_NAME: {
-        Ref: "secrets",
-      },
     },
     deploymentMethod: "direct",
     httpApi: {
@@ -67,7 +64,7 @@ const serverlessConfiguration: AWS = {
             },
             {
               AttributeName: "secretKey",
-              KeyType: "HASH",
+              KeyType: "RANGE",
             },
           ],
           ProvisionedThroughput: {
