df46af17d3fd4be759f8b7941260512144d7b0f4
Sierra Wendt <serigrace@gmail.com>
2023-05-12 08:44:14 -0700

    ATX-2477 | adds types and error handling

--- packages/create-dxp-app/src/utils/generate-local-repo.ts
+++ packages/create-dxp-app/src/utils/generate-local-repo.ts
@@ -1,15 +1,16 @@
 import * as path from "path";
 import { spawnSync } from "child_process";
 import { copyTemplate } from "./copy-template.js";
-import { GenerateAnswers } from "../types";
+import { GenerateAnswers, GeneratorResponse } from "../types";
 
 export const generateLocalRepo = async ({
   repoName,
   packageManager,
   repoDescription,
-}: GenerateAnswers) => {
+}: GenerateAnswers): Promise<GeneratorResponse> => {
   const localRepoName = repoName.toLowerCase().replace(/\s+/g, "-");
+
   spawnSync("mkdir", [path.join(process.cwd(), localRepoName)]);
   process.chdir(localRepoName);
-  copyTemplate(packageManager, { repoName, repoDescription });
+  return await copyTemplate(packageManager, { repoName, repoDescription });
 };
