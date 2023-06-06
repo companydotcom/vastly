e2f9a0b44ccdba2b9adb97d58aae8660fc58e831
jacob.granberry <jacobgranberry@gmail.com>
2023-06-06 08:19:02 -0500

    ATX-2471 | test

--- apps/website/src/pages/notifications/cli-fail.tsx
+++ apps/website/src/pages/notifications/cli-fail.tsx
@@ -1,7 +1,6 @@
 import { Container, Heading } from "@vastly/ui";
 
 export default function CLIFail() {
-  console.log("test");
   return (
     <Container centerContent mt="44" maxW="container.md">
       <Heading size="md" mb="6">
