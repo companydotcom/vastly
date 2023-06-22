362af91149e50d1c3bc4c3a3ee5e873684ecf82e
jacob.granberry <jacobgranberry@gmail.com>
2023-06-22 11:55:03 -0500

    ATX-2543 | fix get tokens log

--- packages/cli/src/util/login/get-tokens.ts
+++ packages/cli/src/util/login/get-tokens.ts
@@ -10,7 +10,7 @@ export async function getTokens(client: Client) {
 
   const location = new URL("https://wave.vastly.is/notifications/cli-");
 
-  output.log("\n");
+  output.spinner.text = "\n";
   output.spinner.start("Waiting for verification\n");
 
   try {
