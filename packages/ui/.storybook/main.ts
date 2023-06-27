c701280a8ef27e8a501885f2224e30af7c9ba7ca
Sierra Wendt <serigrace@gmail.com>
2023-06-27 10:15:29 -0700

    ATX-2545 | adding deployed forms url to ui storybook, testing deployment

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -21,21 +21,35 @@ const config: StorybookConfig = {
   typescript: {
     check: true,
   },
-  refs: {
-    /**
-     * TODO: replace localhost with deployed `forms` url
-     * */
-    "@vastly/forms": {
-      title: "@vastly/forms",
-      url: "http://localhost:9009",
-    },
-    /**
-     * TODO: remove disabled prop once bug fix/package version has been completed
-     * https://github.com/chakra-ui/chakra-ui/pull/7678)
-     * */
-    "@chakra-ui/react": {
-      disable: true,
-    },
+  refs: (config, { configType }) => {
+    if (configType === "DEVELOPMENT") {
+      return {
+        "@vastly/forms": {
+          title: "@vastly/forms development mode",
+          url: "http://localhost:9009/",
+        },
+        /**
+         * TODO: remove disabled prop once bug fix/package version has been completed
+         * https://github.com/chakra-ui/chakra-ui/pull/7678)
+         * */
+        "@chakra-ui/react": {
+          disable: true,
+        },
+      };
+    }
+    return {
+      "@vastly/forms": {
+        title: "@vastly/forms",
+        url: "vastly-forms.vercel.app",
+      },
+      /**
+       * TODO: remove disabled prop once bug fix/package version has been completed
+       * https://github.com/chakra-ui/chakra-ui/pull/7678)
+       * */
+      "@chakra-ui/react": {
+        disable: true,
+      },
+    };
   },
 };
 
