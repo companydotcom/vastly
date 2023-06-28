6c5985b033d068c4d45af5ed1ea47f776d96e04b
Jacob Granberry <jacob.granberry@company-corp.com>
2023-06-28 13:01:58 -0500

    ATX-2543 | Removes unneeded server timeouts (#131)
    * ATX-2543 | changes package name, switches some files
    
    * ATX-2543 | fix test again
    
    * ATX-2543 | fix get tokens log
    
    * ATX-2543 | switch to print rather than log
    
    * ATX-2543 | fix output error
    
    * ATX-2543 | remove unneeded packages from onboarding
    
    * ATX-2543 | more user-service config, whoami command wireup
    
    * ATX-2543 | fix return
    
    * ATX-2543 | remove unnecessary timeouts

--- packages/cli/src/util/login/get-tokens.ts
+++ packages/cli/src/util/login/get-tokens.ts
@@ -11,44 +11,41 @@ export async function getTokens(
 
   const location = new URL("https://wave.vastly.is/notifications/cli-");
 
-  server.setTimeout(3000);
-
   output.spinner.start("Waiting for verification\n");
 
   try {
     const token: Promise<{ token: string }> = new Promise(async (resolve, reject) => {
-      server
-        .once("request", async (req, res) => {
-          // Close the HTTP connection to prevent
-          // `server.close()` from hanging
-          res.setHeader("connection", "close");
+      server.once("request", async (req, res) => {
+        // Close the HTTP connection to prevent
+        // `server.close()` from hanging
+        res.setHeader("connection", "close");
 
-          const parsedUrl = url.parse(req.url || "", true);
-          const { email, token } = parsedUrl.query;
+        const parsedUrl = url.parse(req.url || "", true);
+        const { email, token } = parsedUrl.query;
 
-          if (!email || !token) {
-            res.writeHead(400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Email and token are required in the query string." }));
+        if (!email || !token) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ error: "Email and token are required in the query string." }));
+        }
+
+        if (email && token) {
+          const authResponse = await executeVerify(client, email, token?.toString());
+
+          if (authResponse) {
+            location.pathname += "success";
+            resolve({
+              token: authResponse?.token,
+            });
           }
+        }
 
-          if (email && token) {
-            const authResponse = await executeVerify(client, email, token?.toString());
+        output.spinner.succeed("Verification success");
 
-            if (authResponse) {
-              location.pathname += "success";
-              resolve({
-                token: authResponse?.token,
-              });
-            }
-          }
+        res.statusCode = 302;
+        res.setHeader("location", location.href);
+        res.end();
+      });
 
-          output.spinner.succeed("Verification success");
-
-          res.statusCode = 302;
-          res.setHeader("location", location.href);
-          res.end();
-        })
-        .setTimeout(3000);
       server.once("error", reject);
     });
 
