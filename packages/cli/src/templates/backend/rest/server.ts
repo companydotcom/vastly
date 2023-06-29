1724066a55340ade5abdbc9788033c4eb3a78fcd
nhumai <nhu.mai@company-corp.com>
2023-06-28 23:29:39 -0700

    ATX-2544 | update default queries for local microservice server

--- packages/cli/src/templates/backend/rest/server.ts
+++ packages/cli/src/templates/backend/rest/server.ts
@@ -1,35 +1,36 @@
-import { join } from 'path'
-import { readFileSync } from 'fs'
-import { argv, createServer } from 'prisma-appsync/dist/server'
+import { join } from "path";
+import { readFileSync } from "fs";
+import { argv, createServer } from "prisma-appsync/dist/server";
 
 (async () => {
-    const schema = readFileSync(join(process.cwd(), argv.flags.schema), { encoding: 'utf-8' })
-    const lambdaHandler = await import(join(process.cwd(), argv.flags.handler))
-    const port = argv.flags.port
-    const watchers = argv.flags.watchers ? JSON.parse(argv.flags.watchers) : []
-    const headers = argv.flags.headers ? JSON.parse(argv.flags.headers) : {}
+  const schema = readFileSync(join(process.cwd(), argv.flags.schema), { encoding: "utf-8" });
+  const lambdaHandler = await import(join(process.cwd(), argv.flags.handler));
+  const port = argv.flags.port;
+  const watchers = argv.flags.watchers ? JSON.parse(argv.flags.watchers) : [];
+  const headers = argv.flags.headers ? JSON.parse(argv.flags.headers) : {};
 
-    const defaultQuery = /* GraphQL */`
-        query listPosts {
-            listPosts {
-                id
-                title
-            }
-        }
-            
-        mutation createPost {
-            createPost(data:{ title: "My first post" }) {
-                title
-            }
-        }
-        `
+  const defaultQuery = /* GraphQL */ `
+    query ListUsers {
+      listUsers {
+        id
+        email
+        fullname
+      }
+    }
 
-    createServer({
-        schema,
-        lambdaHandler,
-        defaultQuery,
-        port,
-        watchers,
-        headers,
-    })
-})()
+    mutation createUser {
+      createPost(data: { email: "test-user@vastly.is" }) {
+        email
+      }
+    }
+  `;
+
+  createServer({
+    schema,
+    lambdaHandler,
+    defaultQuery,
+    port,
+    watchers,
+    headers,
+  });
+})();
