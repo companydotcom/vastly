3b58bdd0ce0901d0c88e7686fea478dbc1788546
Charlie <ctbwilhite@gmail.com>
2023-07-05 13:29:11 -0400

    ATX-2544 | fixes wave -V command, adds changeset (#143)
    * ATX-2544 | refactor generate service command, add generate ciam command
    
    * ATX-2544 | Format error, apollo path
    
    * ATX-2544 | Add local dev server for microservice template
    
    * ATX-2544 | changeset
    
    * ATX-2544 | update default queries for local microservice server
    
    * ATX-2544 | changeset
    
    * ATX-2544 | Fix mutation query
    
    * ATX-2544 | fixes wave -V command
    
    * ATX-2544 | fixes wave -V command, adds changeset
    
    * ATX-2544 | fixes wave -V command, adds changeset
    
    ---------
    
    Co-authored-by: nhumai <nhu.mai@company-corp.com>

--- packages/cli/src/util/config/get-package-info.ts
+++ packages/cli/src/util/config/get-package-info.ts
@@ -3,7 +3,13 @@ import fs from "fs-extra";
 import { type PackageJson } from "type-fest";
 
 export function getPackageInfo() {
-  const packageJsonPath = path.join("package.json");
+  let packageJsonPath = "";
+
+  if (fs.existsSync("dist/package.json")) {
+    packageJsonPath = path.join("dist/package.json");
+  } else {
+    packageJsonPath = path.join("package.json");
+  }
 
   return fs.readJSONSync(packageJsonPath) as PackageJson;
 }
