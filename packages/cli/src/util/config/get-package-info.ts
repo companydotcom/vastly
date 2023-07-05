e3030bccd508fbdb81a9099c80010af07c30f208
ctbw <ctbwilhite@gmail.com>
2023-07-05 12:35:22 -0500

    ATX-2544 | fixes wave -V command, adds changeset

--- packages/cli/src/util/config/get-package-info.ts
+++ packages/cli/src/util/config/get-package-info.ts
@@ -3,13 +3,7 @@ import fs from "fs-extra";
 import { type PackageJson } from "type-fest";
 
 export function getPackageInfo() {
-  let packageJsonPath = "";
-
-  if (fs.existsSync("dist/package.json")) {
-    packageJsonPath = path.join("dist/package.json");
-  } else {
-    packageJsonPath = path.join("package.json");
-  }
+  const packageJsonPath = path.join("package.json");
 
   return fs.readJSONSync(packageJsonPath) as PackageJson;
 }
