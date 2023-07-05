f10a361f2c0635dbbaa30ebd414bf19d233bd047
jacob.granberry <jacobgranberry@gmail.com>
2023-07-05 14:24:55 -0500

    ATX-2543 | adds better app root finder

--- packages/cli/src/util/config/get-package-info.ts
+++ packages/cli/src/util/config/get-package-info.ts
@@ -1,9 +1,16 @@
 import path from "path";
 import fs from "fs-extra";
 import { type PackageJson } from "type-fest";
+import { findUpSync } from "find-up";
+
+export function findRoot({ cwd }: any = {}) {
+  const filePath = findUpSync("package.json", { cwd });
+  return filePath && path.dirname(filePath);
+}
 
 export function getPackageInfo() {
-  const packageJsonPath = path.join("package.json");
+  const appRoot = findRoot();
+  const packageJsonPath = path.join(appRoot || "./", "package.json");
 
   return fs.readJSONSync(packageJsonPath) as PackageJson;
 }
