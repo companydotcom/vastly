8d7aa9d0fdaeca6a292847ca23ec9987c0f02b5d
nhumai <nhu.mai@company-corp.com>
2023-07-27 08:09:48 -0700

    ATX-2577 | remove console log

--- packages/wave/src/commands/dev/index.ts
+++ packages/wave/src/commands/dev/index.ts
@@ -7,7 +7,6 @@ export default async function dev(client: Client) {
   const { output } = client;
   try {
     if (existsSync("./prisma") && existsSync("./server.ts")) {
-      console.log("inside");
       execSync(
         'npx prisma generate && DATABASE_URL=\'file:dev.sqlite\' npx prisma db push --accept-data-loss && DATABASE_URL=\'file:dev.sqlite\' npx vite-node ./server.ts --watch -- --schema prisma/generated/prisma-appsync/schema.gql --watchers \'[{"watch":["**/*.prisma","*.prisma"],"exec":"npx prisma generate && DATABASE_URL=\'file:dev.sqlite\' npx prisma db push --accept-data-loss && touch ./server.ts"}]\'',
         { stdio: "inherit" },
