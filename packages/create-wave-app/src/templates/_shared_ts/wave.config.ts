43ff053c91e79a2dde7d5aa81c3796bd2f6e4ff4
ctbw <ctbwilhite@gmail.com>
2023-07-31 14:33:47 -0500

    ATX-2595 | adds wave.config.ts loader

--- packages/create-wave-app/src/templates/_shared_ts/wave.config.ts
+++ packages/create-wave-app/src/templates/_shared_ts/wave.config.ts
@@ -1,11 +1,7 @@
-const dxpConfig = {
-  /**
-   *  What are the configs we need?
-   * @dxp/core is a package that is a dependency of the monorepo template, referenced from the package.json into this config file
-   * Its purpose is to control certain functionality of the monorepo template by us. What is the controller??
-   * Leaning towards constraining the creator to only being able to populate specific templates into specific directories
-   *
-   */
-};
+import { defineConfig } from "@vastly/wave";
 
-module.exports = dxpConfig;
+export default defineConfig({
+  name: "wave",
+  stage: "",
+  accountId: "",
+});
