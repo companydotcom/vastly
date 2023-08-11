e13c5cc2889a1350708779396e962e76864fa2cd
Nhu Mai <84061316+nhu-mai-101@users.noreply.github.com>
2023-08-11 07:58:15 -0700

    ATX-2572 | Update prisma-appsync template (#183)
    * ATX-2572 | Update prisma-appsync microservice template
    
    * ATX-2572 | Output credentials for appsync api

--- packages/wave/src/templates/backend/rest/cdk/src/index.ts
+++ packages/wave/src/templates/backend/rest/cdk/src/index.ts
@@ -1,63 +1,72 @@
 /* eslint-disable no-new */
-import { join } from "path";
-import { App } from "aws-cdk-lib";
-import { AuthorizationType } from "@aws-cdk/aws-appsync-alpha";
-import { kebabCase } from "scule";
-import { AppSyncStack } from "./appsync";
+import { join } from 'path'
+import { App, CfnOutput } from 'aws-cdk-lib'
+import { AuthorizationType } from 'aws-cdk-lib/aws-appsync'
+import { kebabCase } from 'scule'
+import { AppSyncStack } from './appsync'
 
-const app = new App();
+const app = new App()
 
 const serviceName = "Service Name";
 
-new AppSyncStack(app, kebabCase(serviceName), {
+const appSyncStack = new AppSyncStack(app, kebabCase(serviceName), {
   resourcesPrefix: serviceName,
-  schema: join(process.cwd(), "prisma/generated/prisma-appsync/schema.gql"),
-  resolvers: join(process.cwd(), "prisma/generated/prisma-appsync/resolvers.yaml"),
-  function: {
-    code: join(process.cwd(), "handler.ts"),
-    memorySize: 1536,
-    useWarmUp: 0, // useWarmUp > 0 will incur extra costs
-    environment: {
-      NODE_ENV: "production",
-      DATABASE_URL: process.env.DATABASE_URL,
-    },
-    bundling: {
-      minify: true,
-      sourceMap: true,
-      forceDockerBundling: true,
-      commandHooks: {
-        beforeBundling(inputDir: string, outputDir: string): string[] {
-          return [`cp ${inputDir}/prisma/schema.prisma ${outputDir}`];
+    schema: join(process.cwd(), 'prisma/generated/prisma-appsync/schema.gql'),
+    resolvers: join(process.cwd(), 'prisma/generated/prisma-appsync/resolvers.yaml'),
+    function: {
+        code: join(process.cwd(), 'handler.ts'),
+        memorySize: 1536,
+        useWarmUp: 0, // useWarmUp > 0 will incur extra costs
+        environment: {
+            NODE_ENV: 'production',
+            DATABASE_URL: process.env.DATABASE_URL,
         },
-        beforeInstall() {
-          return [];
-        },
-        afterBundling() {
-          return [
-            "npx prisma generate",
-            "rm -rf generated",
+        bundling: {
+            minify: true,
+            sourceMap: true,
+            forceDockerBundling: true,
+            commandHooks: {
+                beforeBundling(inputDir: string, outputDir: string): string[] {
+                    return [`cp ${inputDir}/prisma/schema.prisma ${outputDir}`]
+                },
+                beforeInstall() {
+                    return []
+                },
+                afterBundling() {
+                    return [
+                        'npx prisma generate',
+                        'rm -rf generated',
 
-            // npm + yarn 1.x
-            "rm -rf node_modules/@prisma/engines",
-            "rm -rf node_modules/@prisma/client/node_modules",
-            "rm -rf node_modules/.bin",
-            "rm -rf node_modules/prisma",
-            "rm -rf node_modules/prisma-appsync",
-            "rm -rf node_modules/.pnpm",
-          ];
+                        // npm + yarn 1.x
+                        'rm -rf node_modules/@prisma/engines',
+                        'rm -rf node_modules/@prisma/client/node_modules',
+                        'rm -rf node_modules/.bin',
+                        'rm -rf node_modules/prisma',
+                        'rm -rf node_modules/prisma-appsync',
+                    ]
+                },
+            },
+            nodeModules: ['prisma', '@prisma/client', 'prisma-appsync'],
+            environment: {
+                NODE_ENV: 'production',
+            },
         },
-      },
-      nodeModules: ["prisma", "@prisma/client", "prisma-appsync"],
-      environment: {
-        NODE_ENV: "production",
-      },
     },
-  },
-  authorizationConfig: {
-    defaultAuthorization: {
-      authorizationType: AuthorizationType.API_KEY,
+    authorizationConfig: {
+        defaultAuthorization: {
+            authorizationType: AuthorizationType.API_KEY,
+        },
     },
-  },
-});
+})
 
-app.synth();
+new CfnOutput(appSyncStack, 'AppSyncApiUrlOutput', {
+  value: appSyncStack.getApiUrl(),
+  description: 'AppSync API URL',
+})
+
+new CfnOutput(appSyncStack, 'AppSyncApiKeyOutput', {
+  value: appSyncStack.getApiKey() || '',
+  description: 'AppSync API Key',
+})
+
+app.synth()
