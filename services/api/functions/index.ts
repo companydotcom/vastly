40396a90c142aacba8ceaeedc4431ecd42b26d1f
kumar-nishant <nishant.kumar@accionlabs.com>
2023-09-01 15:04:15 +0530

    DVOPS-998 Uncomment cognito section in api service

--- services/api/functions/index.ts
+++ services/api/functions/index.ts
@@ -3,13 +3,13 @@ import type { AWS } from "@serverless/typescript";
 export const functions: AWS["functions"] = {
   auth: {
     handler: "functions/authorize.handler",
-/*     environment: {
+    environment: {
       USER_POOL_ID: {
         "Fn::ImportValue": "PasswordlessMagicLinksUserPoolArn-${self:provider.stage}",
       },
       APP_CLIENT_ID: {
         "Fn::ImportValue": "WebUserPoolClientArn-${self:provider.stage}",
       },
-    }, */
+    },
   },
 };
