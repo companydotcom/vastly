049380c20386208b9845382b3567a67bb0257a7a
jacob.granberry <jacobgranberry@gmail.com>
2023-10-26 15:16:13 -0500

    ATX-2632 | add note

--- packages/tailwind/tsup.config.ts
+++ packages/tailwind/tsup.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "tsup";
 
+// Need to figure out how to build and package everything correctly
+// https://github.com/egoist/tsup/issues/835
+
 export default defineConfig({
   dts: true,
   target: "es2019",
