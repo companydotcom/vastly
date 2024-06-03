93b8825957cb61f0f3e16cdc7533b4409abb4f16
Sierra Wendt <81435716+ccdlt@users.noreply.github.com>
2024-06-03 11:15:47 -0700

    ATX-2756 | adds pull all command, updates version (#231)
    * ATX-000 | updating versions
    
    * ATX-002 | force publish
    
    * Revert "ATX-002 | force publish (#225)" (#226)
    
    This reverts commit 08d17a7d4fd0f04c44b7f6b1f9680b3f39e106a7.
    
    * ATX-002 | replacing hard code, force publish
    
    * ATX-2756 | adds pull all command, error handling
    
    * ATX-2756 | rebuild
    
    * ATX-2756 | adds pull all command, updates version

--- packages/cli/src/commands/env/pull-all.ts
+++ packages/cli/src/commands/env/pull-all.ts
@@ -1,77 +1,29 @@
-import fastGlob from "fast-glob";
-
 import ora from "ora";
 import chalk from "chalk";
 import { findUp } from "find-up";
 import path from "node:path";
 import { Client } from "../../util/client.js";
-import { getAllEnv, getAppsFromTable } from "../../util/env/pull-all.js";
+import { getAllEnv, getAppsFromTable } from "../../util/env/pull-env-utils.js";
 import writeToFile from "../../util/env/write-env-files.js";
 import { errorToString } from "@vastly/utils";
-import { findOrCreateTable } from "../../util/env/find-or-create-table.js";
 
+/**
+ * The --all flag is used for repo deployment and will write your envs to the root directory of the checked out repository
+ * @param client
+ * @returns
+ */
 export default async function pullAllEnv(client: Client) {
   const rootDir = path.dirname((await findUp(["apps", "packages", "services", "pnpm-workspace.yaml"])) || ".");
-  const allDirs = await fastGlob(["apps/*/", "packages/*/", "services/*/"], {
-    cwd: rootDir,
-    onlyDirectories: true,
-    deep: 1,
-  });
 
   const { output } = client;
   let spinner = output.spinner;
-
   try {
-    spinner = ora({
-      text: `Checking for an env table...\n`,
-      color: "yellow",
-    }).start();
-
-    const tableExists = await findOrCreateTable();
-    if (tableExists) {
-      spinner.succeed(chalk.green("Table Found! \n"));
-
-      const apps = await getAppsFromTable();
-      const answers = await client
-        .prompt([
-          {
-            type: "list",
-            name: "app",
-            message: "Which APP or SERVICE do you want to pull from?",
-            choices: apps,
-            default: apps?.[0],
-          },
-          {
-            type: "input",
-            name: "fileName",
-            message: "Input your env file name:",
-            default: ".env.local",
-          },
-          {
-            type: "checkbox",
-            name: "directory",
-            message: "Choose a directory to write to:",
-            choices: ["root", ...allDirs],
-            validate: (input) => (input.length ? true : "You must choose at least one directory"),
-          },
-        ])
-        .then((a) => a)
-        .catch((error) => {
-          if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-            throw new Error("Interactive mode not supported");
-          } else {
-            // Something else went wrong
-            output.error("something wrong");
-          }
-        });
-
       spinner = ora({
-        text: `Fetching variables for ${answers.app}...\n`,
+        text: `Fetching envs...\n`,
         color: "yellow",
       }).start();
 
-      const response = await getAllEnv(answers.app);
+      const response = await getAllEnv();
       if (response?.length) {
         spinner.succeed(chalk.green(`Success! \n`));
 
@@ -80,7 +32,7 @@ export default async function pullAllEnv(client: Client) {
           color: "magenta",
         }).start();
 
-        const directory = await writeToFile(response, answers.directory, answers.fileName);
+        const directory = await writeToFile(response, ["root"], '.env');
         spinner.succeed(chalk.bgGreenBright(`File successfully created!\n`));
         directory.forEach((dir) =>
           console.log(
@@ -89,12 +41,8 @@ export default async function pullAllEnv(client: Client) {
           ),
         );
       }
-      return;
-    }
-    spinner.fail(chalk.bgMagentaBright("  No table found! Add an env to get started  \n"));
-    throw new Error();
+    return;
   } catch (err: unknown) {
-    spinner.fail();
-    output.error(`Pull: ${errorToString(err)}`);
+    output.error(`Pull Env: ${errorToString(err)}`);
   }
 }
