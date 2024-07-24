0805191b7c01712d1c931ba0ba668c6a85d5ab95
jacob.granberry <jacobgranberry@gmail.com>
2024-07-24 14:39:48 -0500

    ATX-2820 | fix TW flow maybe

--- packages/create-wave-app/src/templates/_shared_ts/apps/client/tailwind.config.ts
+++ packages/create-wave-app/src/templates/_shared_ts/apps/client/tailwind.config.ts
@@ -1,11 +1,1 @@
-// tailwind config is required for editor support
-
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
-
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
-  presets: [sharedConfig],
-};
-
-export default config;
+export * from "@repo/ui/tailwind.config";
