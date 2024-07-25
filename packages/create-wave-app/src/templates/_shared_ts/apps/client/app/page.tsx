34a06967bc3950c5502bb9027e293dd3b1ed0763
jacob.granberry <jacobgranberry@gmail.com>
2024-07-25 09:48:42 -0500

    ATX-2820 | remove shared TW package

--- packages/create-wave-app/src/templates/_shared_ts/apps/client/app/page.tsx
+++ packages/create-wave-app/src/templates/_shared_ts/apps/client/app/page.tsx
@@ -1,4 +1,12 @@
 import { Button } from "@repo/ui/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components/ui/card";
 
 const LINKS = [
   {
@@ -27,17 +35,27 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <div className="w-full max-w-5xl items-center justify-between">
-          <p className="text text-lag">Welcome to Create-Wave-App!</p>
+        <div className="w-full max-w-5xl items-center justify-between mb-6">
+          <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+            Welcome to Create-Wave-App!
+          </h1>
         </div>
-        <div className="bg-white">
-          <Button>TEST BUTTON</Button>
-        </div>
-        <div className="">
+        <div className="grid w-full grid-cols-2 items-center gap-4">
           {LINKS.map((link) => (
-            <div key={link.heading}>
-              <p>{link.heading}</p>
-            </div>
+            <Card className="w-[350px]" key={link.heading}>
+              <CardHeader>
+                <CardTitle>{link.heading}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>{link.description}</CardDescription>
+              </CardContent>
+              <CardFooter>
+                <a className="flex flex-row gap-2" href={link.link} target="_blank">
+                  <span>Read More</span>
+                  <ArrowIcon />
+                </a>
+              </CardFooter>
+            </Card>
           ))}
         </div>
       </div>
@@ -45,11 +63,11 @@ export default function Page(): JSX.Element {
   );
 }
 
-// const ArrowIcon = () => (
-//   <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path
-//       d="M20.5312 13.0781L13.0312 20.5781C12.75 20.8594 12.375 21 12 21C11.5781 21 11.2031 20.8594 10.9219 20.5781C10.3125 20.0156 10.3125 19.0312 10.9219 18.4688L15.8438 13.5H1.5C0.65625 13.5 0 12.8438 0 12C0 11.2031 0.65625 10.5 1.5 10.5H15.8438L10.9219 5.57812C10.3125 5.01562 10.3125 4.03125 10.9219 3.46875C11.4844 2.85938 12.4688 2.85938 13.0312 3.46875L20.5312 10.9688C21.1406 11.5312 21.1406 12.5156 20.5312 13.0781Z"
-//       fill="#2D3748"
-//     />
-//   </svg>
-// );
+const ArrowIcon = () => (
+  <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M20.5312 13.0781L13.0312 20.5781C12.75 20.8594 12.375 21 12 21C11.5781 21 11.2031 20.8594 10.9219 20.5781C10.3125 20.0156 10.3125 19.0312 10.9219 18.4688L15.8438 13.5H1.5C0.65625 13.5 0 12.8438 0 12C0 11.2031 0.65625 10.5 1.5 10.5H15.8438L10.9219 5.57812C10.3125 5.01562 10.3125 4.03125 10.9219 3.46875C11.4844 2.85938 12.4688 2.85938 13.0312 3.46875L20.5312 10.9688C21.1406 11.5312 21.1406 12.5156 20.5312 13.0781Z"
+      fill="#2D3748"
+    />
+  </svg>
+);
